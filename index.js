5cb14bdc6ef918beb49a4fd2dd9ad845ea5681f8
Dominik Salai <dominik.salai@gmail.com>
2016-10-06 22:38:52 +0200

    Implemented application entry point

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(bodyParser.json({ type: '*/*' }));
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log('Server listening on port: ', port);
