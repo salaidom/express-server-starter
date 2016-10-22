bcb2f67c53454fdf3637ef7d88dd0ae9d219caf6
Dominik Salai <dominik.salai@gmail.com>
2016-10-22 10:08:24 +0200

    Restructured for more simple file organisation

---
+++ app/config/config-template.js
@@ -0,0 +1,5 @@
+module.exports = {
+    app: {
+        secret: '<YOUR APPLICATION SECRET HERE>'
+    },
+}
---
+++ app/controllers/authentication.js
@@ -0,0 +1,52 @@
+const jwt = require('jwt-simple');
+const User = require('../model/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user._id, iat: timestamp }, config.secret);
+}
+
+module.exports.signIn = function (req, res, next) {
+    res.status(200).json({
+        token: tokenForUser(req.user)
+    });
+}
+
+module.exports.signUp = function (req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(422).json({
+            error: 'You must provide both email and password!'
+        });
+    }
+
+    User.findOne({ email: email }, function (err, existingUser) {
+        if (err) {
+            return next(err);
+        }
+
+        if (existingUser) {
+            return res.status(422).json({
+                error: 'Email is already in use by another user!'
+            });
+        }
+
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function (err) {
+            if (err) {
+                return next(err);
+            }
+
+            return res.status(200).json({
+                token: tokenForUser(user)
+            });
+        });
+    });
+}
