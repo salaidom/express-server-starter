5291d1d8ad6630b976509422394945adceda0c2f
Dominik Salai <dominik.salai@gmail.com>
2016-10-22 10:18:20 +0200

    Refactored application entry point

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-const http = require('http');
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const router = require('./app/router.js');
-
-const app = express();
-
-mongoose.connect('mongodb://localhost/express-server-starter');
-
-app.use(morgan('combined'));
-app.use(helmet());
-app.use(bodyParser.json({ type: '*/*' }));
-
-router(app);
-
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-
-server.listen(port);
-
-console.log('Server listening on port: ', port);
+/**
+ * APPLICATION ENTRY POINT
+ */
+require('./app/index.js');
