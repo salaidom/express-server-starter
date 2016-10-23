d02a833f5e2689b3f173dcc5772d9483c598b776
Dominik Salai <dominik.salai@gmail.com>
2016-10-23 13:35:25 +0200

    Refactoring the user endpoint

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const _ = require('lodash');
+
 const User = require('../models/user.js');
 
 module.exports.getUsers = function(request, response, next) {
@@ -7,102 +9,67 @@ module.exports.getUsers = function(request, response, next) {
     .then(function(users){
         return response.status(200).json(users)
     })
-    .catch(function(error){
-        return next(error);
-    });
+    .catch(function(error){ return next(error); });
 };
 
 module.exports.createUser = function(request, response, next) {
     const email = request.body.email;
     const password = request.body.password;
 
-    if (!email || !password) {
-        return response.status(422).end('You must provide both email and password');
-    }
+    if (!email || !password) { return response.status(422).end('You must provide both email and password'); }
 
     User.findOne({ email: email })
     .then(function(existingUser) {
-        if (existingUser) {
-            return response.status(422).end('Email is already in use by another user');
-        }
+        if (existingUser) { return response.status(422).end('Email is already in use by another user'); }
 
         const user = new User({ email: email, password: password });
 
         user.save()
-        .then(function() {
-            return response.status(200).end('Ok');
-        })
-        .catch(function(error) {
-            return next(error);
-        });
+        .then(function() { return response.status(200).end('Ok'); })
+        .catch(function(error) { return next(error); });
     })
-    .catch(function(error) {
-        return next(error);
-    });
+    .catch(function(error) { return next(error); });
 };
 
 module.exports.getUser = function(request, response, next) {
     const id = request.params.id;
 
-    if (!id) {
-        return response.status(422).end('You must provide user id');
-    }
+    if (!id) { return response.status(422).end('You must provide user id'); }
 
     User.findById(id)
     .then(function(user) {
-        if (!user) {
-            return response.status(404).end('User not found');
-        }
+        if (!user) { return response.status(404).end('User not found'); }
 
         return response.status(200).json(user)
     })
-    .catch(function(error) {
-        return next(error);
-    });
+    .catch(function(error) { return next(error); });
 };
 
 module.exports.updateUser = function(request, response, next) {
     const id = request.params.id;
+    const userData = _.omit(_.extend({}, request.body), ['_id', 'password']);
 
-    if (!id) {
-        return response.status(422).end('You must provide user id');
-    }
+    if (!id) { return response.status(422).end('You must provide user id'); }
 
-    const user = User.findById(id)
+    User.findByIdAndUpdate(id, userData, { new: true })
     .then(function(user) {
-        if (!user) {
-            return response.status(404).end('User not found');
-        }
+        if (!user) { return response.status(404).end('User not found'); }
 
         return response.status(200).json(user)
     })
-    .catch(function(error) {
-        return next(error);
-    });
+    .catch(function(error) { return next(error); });
 };
 
 module.exports.deleteUser = function(request, response, next) {
     const id = request.params.id;
 
-    if (!id) {
-        return response.status(422).end('You must provide user id');
-    }
+    if (!id) { return response.status(422).end('You must provide user id'); }
 
-    const user = User.findById(id)
+    User.findByIdAndRemove(id)
     .then(function(user) {
-        if (!user) {
-            return response.status(404).end('User not found');
-        }
+        if (!user) { return response.status(404).end('User not found'); }
 
-        user.remove()
-        .then(function() {
-            return response.status(200).end('Ok');
-        })
-        .catch(function(error) {
-            return next(error);
-        });
+        return response.status(200).json(user)
     })
-    .catch(function(error) {
-        return next(error);
-    });
+    .catch(function(error) { return next(error); });
 };
